8963a2c513461daa8a11492415f3f1d385e7139b
NguyenAnhQuan1412 <NguyenAnhQuan1412@users.noreply.github.com>
2018-09-27 22:57:28 +0700

    Update1

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -4,12 +4,19 @@ public class Week3 {
 
     public static int max(int m, int n) {
         // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+		if(m>n) return m;
+		else return n;
     }
 
     public static int minOfArray(int[] array) {
         // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+		int i;
+		int min;
+		min=array[0];
+		for(i=1;array[i]!=null;i++){
+			if(array[i]<min) min=array[i];
+		}
+        return min;
     }
 
     /**
@@ -20,6 +27,15 @@ public class Week3 {
      */
     public static String calculateBMI(double weight, double height) {
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
+		double bmi;
+		bmi=weight/(height*height);
+		if(bmi>25) return "Béo phì";
+		else{
+			if(bmi>=23) return "Thừa cân";
+			else{
+				if(bmi>=18.5) return "Bình thường";
+				else return "Thiếu cân";
+			}
+		}
     }
 }
